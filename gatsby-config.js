671a14ba61520a11e130600dad08e1cedff61e45
vstds <info@vstpress.com>
2020-10-05 21:24:29 +0530

    remove starter files

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,16 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0088C8`,
+        theme_color: `#0088C8`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
